a76c2cb33343fcdf8406f5d4975b322b2e1315f2
TakingTheL <yliu6@stuy.edu>
2017-01-24 20:13:31 -0500

    you didn't import ArrayList...

--- APStudent.java
+++ APStudent.java
@@ -1,4 +1,7 @@
+import java.util.*;
+    
 public class APStudent {
+
 
     ArrayList<APExam> exams;
 
@@ -34,7 +37,7 @@ public class APStudent {
 	}
     }
 	
-    public ArrayList<Double> percentages ( ArrayList<APStudents> list ) {
+    public ArrayList<Double> percentages ( ArrayList<APStudent> list ) {
 	int n = 0;
 	int s = 0;
 	int h = 0;
@@ -49,7 +52,7 @@ public class APStudent {
 		h += 1;
 	    }
 	}
-	ArrayList<Double> list1 = new ArrayList<Double>(3);
+	ArrayList<Double> list1 = new ArrayList<Double>();
 	list.set(0, ( (double) n * 100 / list.size() ) );
 	list.set(1, ( (double) s * 100 / list.size() ) );
 	list.set(2, ( (double) h * 100 / list.size() ) );
