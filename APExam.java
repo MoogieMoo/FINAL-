2bad8445d5b0913d2df95b2dac7f23c9600313f5
MoogieMoo <yxia1@stuy.edu>
2017-01-24 20:31:37 -0500

    Update APExam.java

--- APExam.java
+++ APExam.java
@@ -1,21 +1,18 @@
 public class APExam {
-
     String subject;
     int level;
     double grade;
-
     public APExam( int newLevel, double newGrade ) {
-	level = newLevel;
-	grade = newGrade;
+			level = newLevel;
+			grade = newGrade;
     }
 	
-
     public int getLevel() {
-	return level;
+			return level+0;
     }
-
+    //~security~
     public double getGrade() {
-	return grade;
+			return grade+0.0;
     }
 
 }
